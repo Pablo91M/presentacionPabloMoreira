4e6b01d64f44653e7bc2594b8bd732d441904c59
Pablo91M <pabloruben12@gmail.com>
2023-03-26 21:22:07 -0300

    ultima

--- src/apps.js
+++ src/apps.js
@@ -1,5 +1,5 @@
 const express = require ('express'); //trae la libreria 
 const app = express();// ejecuta funcion del codigo
 const PORT = 3001; //puerto
-app.get("/api", (req,res) => {res.json({mensaje: 'hola mundo'}).status(200)});
+app.get("/api", (req,res) => {res.json({mensaje: 'hola mundo!!!!'}).status(200)});
 app.listen(PORT,() => {console.log('estoy escuchando en el puerto:' + PORT)});
